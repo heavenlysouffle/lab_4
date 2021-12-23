bae85adc0e404d25fa096bac01a6f5b3c029c37e
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-12-23 08:35:43 +0200

    added TeacherFactory

--- task_3/factories.py
+++ task_3/factories.py
@@ -1,5 +1,5 @@
-from classes import LocalCourse, OffsiteCourse
-from interfaces import ICourseFactory
+from classes import LocalCourse, OffsiteCourse, Teacher
+from interfaces import ICourseFactory, ITeacherFactory
 
 
 class CourseFactory(ICourseFactory):
@@ -10,3 +10,9 @@ class CourseFactory(ICourseFactory):
     @staticmethod
     def create_offsite(name, address, *program):
         return OffsiteCourse(name, address, *program)
+
+
+class TeacherFactory(ITeacherFactory):
+    @staticmethod
+    def create_teacher(surname, name, patronymic, birth_date):
+        return Teacher(surname, name, patronymic, birth_date)
