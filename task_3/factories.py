9c4eb34c012336f06cc27a675602a6bbb5cc9823
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-12-23 17:38:52 +0200

    added documentation, annotations

--- task_3/factories.py
+++ task_3/factories.py
@@ -3,16 +3,23 @@ from interfaces import ICourseFactory, ITeacherFactory
 
 
 class CourseFactory(ICourseFactory):
+    """Factory to create offsite and local courses"""
+
     @staticmethod
-    def create_local(name, room, *program):
+    def create_local(name: str, room: int, *program: list[str]) -> LocalCourse:
+        """Returns created local course"""
         return LocalCourse(name, room, *program)
 
     @staticmethod
-    def create_offsite(name, address, *program):
+    def create_offsite(name: str, address: str, *program: list[str]) -> OffsiteCourse:
+        """Returns created offsite course"""
         return OffsiteCourse(name, address, *program)
 
 
 class TeacherFactory(ITeacherFactory):
+    """Factory to create teacher profile"""
+
     @staticmethod
-    def create_teacher(surname, name, patronymic, birth_date):
+    def create_teacher(surname: str, name: str, patronymic: str, birth_date: str) -> Teacher:
+        """Returns created Teacher profile"""
         return Teacher(surname, name, patronymic, birth_date)
