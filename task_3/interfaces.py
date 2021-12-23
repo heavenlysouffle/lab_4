c33629aaf5e19db1076cf78be6729afdf27e30b9
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-12-23 08:36:17 +0200

    added ITeacherFactory

--- task_3/interfaces.py
+++ task_3/interfaces.py
@@ -1,6 +1,61 @@
 from abc import ABC, abstractmethod
 
 
+class ITeacher(ABC):
+    @property
+    @abstractmethod
+    def surname(self):
+        pass
+
+    @surname.setter
+    @abstractmethod
+    def surname(self, surname):
+        pass
+
+    @property
+    @abstractmethod
+    def name(self):
+        pass
+
+    @name.setter
+    @abstractmethod
+    def name(self, name):
+        pass
+
+    @property
+    @abstractmethod
+    def patronymic(self):
+        pass
+
+    @patronymic.setter
+    @abstractmethod
+    def patronymic(self, patronymic):
+        pass
+
+    @property
+    @abstractmethod
+    def birth_date(self):
+        pass
+
+    @birth_date.setter
+    @abstractmethod
+    def birth_date(self, birth_date):
+        pass
+
+    @property
+    @abstractmethod
+    def courses(self):
+        pass
+
+    @abstractmethod
+    def add_course(self, course):
+        pass
+
+    @abstractmethod
+    def remove_course(self, course):
+        pass
+
+
 class ICourse(ABC):
     @property
     @abstractmethod
@@ -72,4 +127,11 @@ class ICourseFactory(ABC):
     @staticmethod
     @abstractmethod
     def create_offsite(name, address, *program):
+        pass
+
+
+class ITeacherFactory(ABC):
+    @staticmethod
+    @abstractmethod
+    def create_teacher(surname, name, patronymic, birth_date):
         pass
